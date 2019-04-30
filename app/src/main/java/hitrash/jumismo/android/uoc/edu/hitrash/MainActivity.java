0815d3dfa11d79fa19f3cb3c1b3cce7e7abf2944
Juan Miguel <jumismo@uoc.edu>
2019-05-01 01:31:14 +0200

    Primer commit. Ya se ha creado las vistas de inicio, login y registro

---
+++ app/src/main/java/hitrash/jumismo/android/uoc/edu/hitrash/MainActivity.java
@@ -0,0 +1,40 @@
+package hitrash.jumismo.android.uoc.edu.hitrash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private Button buttonRegister;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+}
